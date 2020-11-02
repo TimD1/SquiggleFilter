0319021b746b3174c23cf66b02e4581a4a04eab5
Tim Dunn <me.timd1@gmail.com>
2020-11-01 20:57:50 -0500

    Added image generation scripts and image
     - plot_range.py: plots histogram of 13-bit ADC sampled data
     - virus_genomes.py: plots typical virus genome sizes
     - scaled_reads/ and unscaled_reads/ contain examples with/out med/mad norming

---
+++ img/plot_range.py
@@ -0,0 +1,20 @@
+import h5py
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+
+
+file_name = "/home/timdunn/datasets/lambdaphage/FAL11227_8bb4695a85a5e3a621c58f18f47290ee50a12ebc_0.fast5"
+fast5_file = h5py.File(file_name, 'r')                  
+
+# get signal from single-FAST5 file                                      
+all_signals = []
+for read_name in fast5_file:
+    signal = np.array(fast5_file[read_name]['Raw']['Signal'][:], dtype=np.int32)
+    signal += int(fast5_file[read_name]['channel_id'].attrs['offset'])
+    all_signals.extend(signal)
+
+plt.hist(all_signals, bins=100, range=[0,1000], facecolor='g', alpha=0.6)
+plt.xlabel('ADC Value')
+plt.ylabel('Counts')
+plt.savefig('signal_range.png')
