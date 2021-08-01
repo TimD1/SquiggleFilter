58f87c6b1b2545ed2d5a2cd9b8d8149478bbac68
Tim Dunn <me.timd1@gmail.com>
2021-07-31 21:23:53 -0400

    Added newlines to setup script.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 G1='\033[0;32m'
 G2='\033[0m'
 
-echo -e "${G1}installing aws s3 (for downloading datasets)${G2}"
+echo -e "\n${G1}installing aws s3 (for downloading datasets)${G2}"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" \
     -o "aws_cli_v2.zip" && \
     unzip aws_cli_v2.zip && \
@@ -12,7 +12,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" \
     rm -rf ./aws
 echo -e "${G1}done!${G2}"
 
-echo -e "${G1}downloading covid data...${G2}"
+echo -e "\n${G1}downloading covid data...${G2}"
 mkdir -p data/covid/fast5 && \
     cd data/covid/fast5 && \
     wget https://cadde.s3.climb.ac.uk/SP1-raw.tgz && \
@@ -21,14 +21,14 @@ mkdir -p data/covid/fast5 && \
     cd -
 echo -e "${G1}done!${G2}"
 
-echo -e "${G1}downloading human data...${G2}"
+echo -e "\n${G1}downloading human data...${G2}"
 mkdir -p data/human/fast5
 aws s3 --no-sign-request sync \
         s3://ont-open-data/gm24385_2020.09/analysis/r9.4.1/20200914_1357_1-E11-H11_PAF27462_d3c9678e/guppy_v4.0.11_r9.4.1_hac_prom/align_unfiltered/chr19/fast5/ \
         data/human/fast5  
 echo -e "${G1}done!${G2}"
 
-echo -e "${G1}setting up virtual environment...${G2}"
+echo -e "\n${G1}setting up virtual environment...${G2}"
 python3.6 -m venv sf-venv3
 source sf-venv3/bin/activate
 pip install --upgrade pip
